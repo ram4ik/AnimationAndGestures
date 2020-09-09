da489e0a3be457152215e5c694f05a4d7a78bdd3
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-09-09 09:53:07 +0300

    Initial Commit

---
+++ AnimationAndGestures/AnimationAndGesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationAndGesturesApp.swift
+//  AnimationAndGestures
+//
+//  Created by ramil on 09.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct AnimationAndGesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationAndGestures/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimationAndGestures
+//
+//  Created by ramil on 09.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
